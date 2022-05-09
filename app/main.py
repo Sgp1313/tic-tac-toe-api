0da487556f6c21ca8c1c6c85be6512c5b334c1ae
Angel Tórtola <tortola@ufm.edu>
2022-05-09 13:39:47 -0600

    [fix] Multiple bugs fixed

--- app/main.py
+++ app/main.py
@@ -12,21 +12,27 @@ board = utils.create_empty_board()
 players = [None, None] 
 turn = None
 winner = None
+# Print initial board
 utils.print_board(board)
 
 
 @app.get("/registry", tags=["tic-tac-toe"])
 def is_registry_open() -> bool:
+    
+    global turn
 
     if turn is None:
+        print("Current players:", players)
         return True
     
+    print("Current players:", players)
     return False
 
 
 @app.post("/register_player/{name}", tags=["tic-tac-toe"])
 def register_player(name: str) -> any:
 
+    global players, turn
     x = players[0]
     o = players[1]
   
@@ -36,25 +42,31 @@ def register_player(name: str) -> any:
         players[symbol] = name
 
         if symbol == 0:
+            print("Current players:", players)
             return "X"
         
+        print("Current players:", players)
         return "O"
 
     if x is None:
         players[0] = name
+        print("Current players:", players)
 
-        if x != None and o != None:
+        if players[0] is not None and players[1] is not None:
             # Both players are ready to play, turn is set to "X"
             turn = "X"
+            print("Current turn:", turn)
 
         return "X"
 
     if o is None: 
         players[1] = name
+        print("Current players:", players)
 
-        if x != None and o != None:
-            # Both players are ready to play, turn is set to "X"
+        if players[0] is not None and players[1] is not None:
+            # Both players are ready to play, turn is set to "X"          
             turn = "X"
+            print("Current turn:", turn)
 
         return "O"
 
@@ -63,6 +75,9 @@ def register_player(name: str) -> any:
 
 @app.get("/turn/{player_id}", tags=["tic-tac-toe"])
 def get_player_turn(player_id: str) -> bool:
+    
+    global turn
+    print("Current turn:", turn)
     
     if player_id == "X" and turn == "X":
         return True
@@ -76,14 +91,19 @@ def get_player_turn(player_id: str) -> bool:
 @app.get("/board", tags=["tic-tac-toe"])
 def get_board():
 
+    global board
+
     return board
 
 
 @app.post("/move/{player_id}/{row}/{column}", tags=["tic-tac-toe"])
-def make_move(player_id: str, row: int, column: int):
+def make_move(player_id: str, row: int, column: int) -> bool:
     
+    global board, turn, winner
     # Updates board with move received
     board = utils.update_board(board, player_id, row, column)
+    # Prints new board
+    utils.print_board(board)
     # Checks for any winner
     x_has_won = utils.check_for_winner(board, "X")
     o_has_won = utils.check_for_winner(board, "O")
@@ -92,22 +112,37 @@ def make_move(player_id: str, row: int, column: int):
 
         if x_has_won:
             winner = "X"
+            print("\n\nGAME OVER, winner: X\n\n")
         else:
             winner = "O"
+            print("\n\nGAME OVER, winner: O\n\n")
 
-        sleep(500)
-
-    #Change turn
+    #Changes current turn
     if turn == "X":
         turn = "O"
-    
-    if turn == "O":
+    elif turn == "O":
         turn = "X"
+
+    print("Current turn:", turn)
+    return True
         
 
 @app.get("/winner/{player_id}", tags=["tic-tac-toe"])
-def get_winner(player_id: str):
+def get_winner(player_id: str) -> bool:
+
+    global board
 
     is_winner = utils.check_for_winner(board, player_id)
 
     return is_winner
+
+
+@app.get("/continue", tags=["tic-tac-toe"])
+def does_game_continue():
+
+    global winner 
+
+    if winner is None:
+        return True 
+    
+    return False
